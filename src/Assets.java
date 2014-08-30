04e85e107b1ea303293811e31c5ae631a0a4181e
exitcode0 <exitcode.zero@gmail.com>
2014-08-30 15:45:00 -0400

    More organization

--- src/Assets.java
+++ src/Assets.java
@@ -1,3 +1,15 @@
+public class AssetOGG{
+  public static Clip getSound(String path){
+    try{
+      Clip sound = (Clip)AudioSystem.getLine(new Line.Info(Clip.class));
+      sound.open(AudioSystem.getAudioInputStream(new File(path + EXTENSION_AUDIO)));
+      return sound;
+    }catch(Exception error){return null;}
+  }
+  public static void playSound(String path){
+    getSound(path).start();
+  }
+}
 import java.net.*;
 import java.io.*;
 import java.util.*;
